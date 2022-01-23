4b6f89a30c47fbb8f72cec366da6060746abced7
Johanderson <johander1822@gmail.com>
2022-01-23 23:05:48 +0100

    very basic cython setup

--- setup.py
+++ setup.py
@@ -1,8 +1,46 @@
 """Setup module."""
 
+import os
 import re
-from setuptools import setup
+
 from pkg_resources import parse_requirements
+from setuptools import Extension, setup
+
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    cythonize = None
+
+
+# https://cython.readthedocs.io/en/latest/src/userguide/source_files_and_compilation.html#distributing-cython-modules
+def no_cythonize(extensions, **_ignore):
+    for extension in extensions:
+        sources = []
+        for sfile in extension.sources:
+            path, ext = os.path.splitext(sfile)
+            if ext in (".pyx", ".py"):
+                if extension.language == "c++":
+                    ext = ".cpp"
+                else:
+                    ext = ".c"
+                sfile = path + ext
+            sources.append(sfile)
+        extension.sources[:] = sources
+    return extensions
+
+
+extensions = [
+    Extension("aiosonic.http_parser", ["aiosonic/http_parser.pyx"]),
+    Extension("aiosonic.connection", ["aiosonic/connection.pyx"]),
+]
+
+CYTHONIZE = bool(int(os.getenv("CYTHONIZE", 0))) and cythonize is not None
+
+if CYTHONIZE:
+    compiler_directives = {"language_level": 3, "embedsignature": True}
+    extensions = cythonize(extensions, compiler_directives=compiler_directives)
+else:
+    extensions = no_cythonize(extensions)
 
 
 def read_file(filename):
@@ -73,6 +111,7 @@ setup(
     ],
     setup_requires=["pytest-runner"],
     install_requires=requirements("./requirements.txt"),
+    ext_modules=extensions,
     extras_require={
         "test": add_marks(
             requirements("./test-requirements.txt"),
